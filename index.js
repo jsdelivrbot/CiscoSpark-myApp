8ba9512ee852655c7f8091ac7381d38f7bcbd3a2
Katherine Chen <cykatherinee@hotmail.com>
2018-01-18 09:58:44 +1100

    Use `socket.io` to connect backend and frontend for `connect` button

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('socket.io test connection');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('authenticate', function(){
+    console.log('Trying to authenticate...');
+    // initiate the login sequence if not authenticated.
+    spark.authorization.initiateLogin();
+  });
 });
 
 http.listen(3000, function(){
