e7d529d708067f7f122f68582984ebe52ae5b78d
Katherine Chen <cykatherinee@hotmail.com>
2018-01-22 10:08:06 +1100

    Cleanup the original contents

--- index.js
+++
@@ -1,9 +0,0 @@
-var spark = require('ciscospark/env');
-spark.rooms.create({
-  title: `My First Room!`
-})
-// Make sure to log errors in case something goes wrong.
-  .catch(function(reason) {
-    console.error(reason);
-    process.exit(1);
-  });
