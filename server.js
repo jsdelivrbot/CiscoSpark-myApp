ec2b1a0607b05e39e27bd57a9973bbbb268aa254
Katherine Chen <cykatherinee@hotmail.com>
2018-01-30 16:06:50 +1100

    Include `body-parser` to get the body from post

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// This will inlcude a body in the request when getting post
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+app.post('/', function (req, res) {
+  console.log("We got a webhook WEEEE!");
+
+  var messageId = req.body.data.id;
+  var personId = req.body.data.personId;
+  socket.emit('messageReceived');
+
+  res.end();
+})
 
 io.on('connection', function(socket){
   console.log('socket.io test connection');
 
-  // socket.on('disconnect', function(){
-  //   console.log('user disconnected');
-  // });
-
-  // socket.on('authenticate', function(){
-  //   console.log('Trying to authenticate...');
-  //   // initiate the login sequence if not authenticated.
-  //   spark.authorization.initiateLogin();
-  // });
+  socket.emit('messageReceived');
 });
 
 http.listen(3000, function(){
